705777464bd0a7981e0ee6278eaa3e3707a0b161
beak <tadanmf@gmail.com>
2017-12-06 11:21:26 +0900

    spring, gradle dependencise 세팅

---
+++ src/main/java/com/task03/WebAppInitializer.java
@@ -0,0 +1,56 @@
+package com.task03;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	Logger log = LoggerFactory.getLogger(WebAppInitializer.class);
+	
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return characterEncodingFilter;
+	}
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		System.out.println("건강검진 안뇽~");
+		log.info("스프링 시작1");
+		
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(WebConfig.class);
+		ctx.setServletContext(servletContext);
+
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+}
